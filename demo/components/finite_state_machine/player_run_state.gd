f95cee59c8d120eb2a04d10f4eaf4a986fbeefc5
Adam Gulacsi <adam.gulacsi@gmail.com>
2026-07-13 08:13:57 +0200

    cleaned up FSM demo code

--- demo/components/finite_state_machine/player_run_state.gd
+++ demo/components/finite_state_machine/player_run_state.gd
@@ -19,13 +19,12 @@ func physics_update(_delta: float) -> void:
 
 	var direction := Input.get_axis("move_left", "move_right")
 
-	if not is_zero_approx(direction):
-		player.velocity.x = direction * player.move_speed
-		player.animated_sprite_2d.flip_h = direction < 0.0
-	else:
+	if is_zero_approx(direction):
 		stopped.emit()
 		return
 
+	player.velocity.x = direction * player.move_speed
+	player.animated_sprite_2d.flip_h = direction < 0.0
 	player.move_and_slide()
 
 	if not player.is_on_floor():
